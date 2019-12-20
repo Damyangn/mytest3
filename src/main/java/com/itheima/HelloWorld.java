89a6645a1ffdac9a848e2558c520d8eed9a84afb
administrator <fengyesuixiang@gmail.com>
2019-12-20 12:51:57 +0800

    提交到本地仓库

---
+++ src/main/java/com/itheima/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+/**
+ * author: Yangn
+ * date : 2019/12/20 11:15
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        String s = "我喜欢篮球";
+    }
+}
